d4db1b7b5ac9c021a72351b9f0c70d857df79e51
Ceili Cornelison <ceili@ceilicornelison.com>
2013-10-24 17:15:47 -0500

    Some old uncommited changes, also started on web interface

--- code.py
+++ code.py
@@ -3,3 +3,12 @@ import web
 urls = (
 	'/', 'index'
 )
+
+class index:
+	def GET(self):
+		return "Hello, world!"
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
+
--- simple_english.py
+++ simple_english.py
@@ -1,13 +1,15 @@
 import sys
 import nltk.data
-from nltk.tokenize import wordpunct_tokenize
+from nltk.tokenize import whitespace_tokenize
+import simple_tokens
 
 input_file = open(sys.argv[1]).read()
-input_file = wordpunct_tokenize(input_file)
+input_file = simple_tokens.simple_tokens(input_file)
+input_file = whitespace_tokenize(input_file)
 input_file = [w.lower() for w in input_file if w.isalpha()]
 basic_english = nltk.corpus.words.words('en-basic')
 
-nonsimple_words = set(input_file) - set(basic_english)
+nonsimple_words = tuple(set(input_file) - set(basic_english))
 print("Total number of words: ")
 print(len(input_file))
 print("Nonsimple words: ")
@@ -16,3 +18,4 @@ print(nonsimple_words)
 percent = 100 - (float(len(nonsimple_words)) / float(len(input_file))) * 100
 print("Percentage simple english: (v0.1)")
 print(percent)
+
