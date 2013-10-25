4026819709f7671dde68e1ec911afa5145729aeb
Ceili Cornelison <ceili@ceilicornelison.com>
2013-10-24 22:20:04 -0500

    Yeahhh. Have this running on a server and I feel awesome :)

--- www/passenger_wsgi.py
+++ www/passenger_wsgi.py
@@ -1,17 +1,19 @@
+from cgi import parse_qs, escape
+
+html = """
 <html lang="en"><head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="assets/ico/favicon.png">
 
     <title>Jumbotron Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="http://www.ceilicornelison.com/sep/assets/css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="assets/css/custom.css" rel="stylesheet">
+    <link href="http://www.ceilicornelison.com/sep/assets/css/custom.css" rel="stylesheet">
 
   </head>
 
@@ -30,24 +32,24 @@
       <div class="row">
         <div class="col-lg-8">
           <h2>Put your text here:</h2>
-          <form role="form">
+          <form role="form" method="post" action="parsing_post.wsgi">
             <div class="form-group">
-              <textarea id="form-input" class="form-control" rows="10"></textarea>
+              <textarea id="notsimple" class="form-control" name="notsimple" rows="10"></textarea>
             </div>
-            <button type="submit" class="btn btn-custom btn-lg">View details »</button>
+            <button type="submit" class="btn btn-custom btn-lg">View details &raquo;</button>
           </form>
         </div>
         <div id="result-column" class="col-lg-4">
           <h2>Result</h2>
-          <p class="result-percent">70%</p>
-          <p>Your English is about 70% simple.</p>
+          <p class="result-percent">%s</p>
+          <p>Your English is about %%%s simple.</p>
        </div>
       </div>
 
       <hr>
 
       <footer>
-        <p>© 2013 by <a href="http://www.ceilicornelison.com/">Ceili Cornelison</a></p>
+        <p>&copy; 2013 by <a href="http://www.ceilicornelison.com/">Ceili Cornelison</a></p>
         <p>Simple English Parser is licensed under a <a href="http://opensource.org/licenses/isc-license.txt">ISC License.</a></p>
       </footer>
     </div> <!-- /container -->
@@ -57,7 +59,35 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="http://www.ceilicornelison.com/sep/assets/js/bootstrap.min.js"></script>
 
 </body>
 </html>
+"""
+def application(environ, start_response):
+
+   # the environment variable CONTENT_LENGTH may be empty or missing
+   try:
+      request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+   except (ValueError):
+      request_body_size = 0
+
+   # When the method is POST the query string will be sent
+   # in the HTTP request body which is passed by the WSGI server
+   # in the file like wsgi.input environment variable.
+   request_body = environ['wsgi.input'].read(request_body_size)
+   d = parse_qs(request_body)
+
+   notsimple = d.get('notsimple', [''])[0]
+
+   notsimple = escape(notsimple)
+
+   response_body = html % (notsimple or 'Empty', notsimple or 'Empty')
+
+   status = '200 OK'
+
+   response_headers = [('Content-Type', 'text/html'),
+                  ('Content-Length', str(len(response_body)))]
+   start_response(status, response_headers)
+
+   return [response_body]
